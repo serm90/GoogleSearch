61bd6a0e7b3fa528f9a7167ecac27f8f19ff7505
serg-qa <sergio.molchanov88@gmail.com>
2017-10-24 11:36:36 +0300

    Initial commit

---
+++ src/test/java/selt2/SecTest.java
@@ -0,0 +1,52 @@
+package selt2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SecTest {
+
+    private static WebDriver driver;
+
+    @BeforeTest
+    public static void setDriver(){
+        System.setProperty("webdriver.chrome.driver","C://Users//serg-qa//Downloads/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.google.com.ua");
+    }
+    @Test
+    public void userSearch() throws InterruptedException {
+
+        WebElement searchField = driver.findElement(By.xpath(".//*[@id='lst-ib']"));
+        searchField.sendKeys("коллайдер");
+        searchField.submit();
+
+        List <WebElement> searchRez1 = driver.findElements(By.xpath("//h3[@class='r']/a"));
+        for (int i = 0; i < searchRez1.size(); i++) {
+            System.out.println(searchRez1.get(i).getText());
+            System.out.println(searchRez1.get(i).getAttribute("href"));
+            }
+
+            driver.get(searchRez1.get(6).getAttribute("href"));
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @AfterTest
+    public void tearDown(){driver.quit();}
+
+}
